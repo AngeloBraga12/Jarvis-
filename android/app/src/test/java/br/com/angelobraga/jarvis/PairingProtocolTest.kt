46f3091f3f639498b380a61c7ddcad4d51cd89a9
Angelo_braga <128182753+AngeloBraga12@users.noreply.github.com>
2026-09-15 18:40:23 -0300

    test(android): cover secure session state transitions

--- android/app/src/test/java/br/com/angelobraga/jarvis/PairingProtocolTest.kt
+++ android/app/src/test/java/br/com/angelobraga/jarvis/PairingProtocolTest.kt
@@ -2,6 +2,7 @@ package br.com.angelobraga.jarvis
 
 import java.util.Base64
 import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
 
@@ -28,5 +29,33 @@ class PairingProtocolTest {
         assertThrows(IllegalArgumentException::class.java) {
             PairingProof("device-1", nonce, "")
         }
+    }
+
+    @Test
+    fun sessionStateMachineRequiresPairingBeforePaired() {
+        val session = DeviceSessionController()
+        assertThrows(IllegalArgumentException::class.java) {
+            session.paired("device-1")
+        }
+        session.beginPairing()
+        session.paired("device-1")
+        assertEquals(DeviceSession.Paired("device-1"), session.state)
+    }
+
+    @Test
+    fun revokedSessionCanOnlyReturnToUnpaired() {
+        val session = DeviceSessionController()
+        session.revoke()
+        assertEquals(DeviceSession.Revoked, session.state)
+        session.reset()
+        assertEquals(DeviceSession.Unpaired, session.state)
+    }
+
+    @Test
+    fun failureMessageIsBounded() {
+        val session = DeviceSessionController()
+        session.fail("x".repeat(1000))
+        val state = session.state as DeviceSession.ConnectionError
+        assertEquals(256, state.message.length)
     }
 }
